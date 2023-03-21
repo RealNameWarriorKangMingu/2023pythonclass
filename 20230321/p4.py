99c657a1d395f61e920679fbd7f93d752c073def
RealNameWarriorKangMingu <rkdalsrn0528@naver.com>
2023-03-21 15:25:32 +0900

    commited

--- 20230321/p4.py
+++ 20230321/p4.py
@@ -1,1 +1,12 @@
 import random
+
+com = random.randrange(3)
+user = int(input('가위 0 바위 1 보 2 선택 : '))
+print('user = %d, com = %d ' % (user, com))
+
+if user == com :
+    print('비겼습니다')
+elif (user == 0 and com == 2) or (user == 1 and com == 0) or (user == 2 and com == 1):
+    print('이겼습니다')
+else :
+    print('패배했습니다')
